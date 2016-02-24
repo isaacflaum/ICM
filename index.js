f0997653fbd424994ca3fe4dff3de37f0d63bfe1
isaac <isaac@forensiq.com>
2016-02-24 00:35:12 -0500

    for better formatting

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 These values can be replaced with anything. I am also currently only
 calculating 2 places being paid which is sufficient for the tournament
 structure which I am emulating, Merge 6Max Hyper Turbo $3.50 buyin.
+
+Note that I am using the 'old' MH algorithm rather than the new
+Ben Roberts algo
 */
 const FIRST_PLACE_PRIZE = 12.76;
 const SECOND_PLACE_PRIZE = 6.86;
 const CHIP_TOTAL = 1800;
-const STACKS = [1200, 400, 200];
+const STACKS = [1100, 500, 100, 100];
 
 // calculate probability of coming in first
 const p1st = STACKS.map(function (stack) {
@@ -31,7 +34,9 @@ let evs = [];
 for (var i = 0; i < STACKS.length; i++) {
 	var expectedVal1st = FIRST_PLACE_PRIZE * p1st[i];
 	var expectedVal2nd = SECOND_PLACE_PRIZE * p2nd[i];
-	evs.push(expectedVal1st + expectedVal2nd);
+	evs.push((expectedVal1st + expectedVal2nd).toPrecision(
+		FIRST_PLACE_PRIZE.toString().length
+	));
 }
 
-console.log(evs);
+console.log(evs); // [ '9.9034', '6.7631', '1.4768', '1.4768' ]
